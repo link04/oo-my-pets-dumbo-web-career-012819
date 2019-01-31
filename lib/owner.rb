d8f2de0bb041152c1ac7e66b748d678f979af4b8
Maximo Bautista <max.bautista97@gmail.com>
2019-01-31 20:20:50 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -57,9 +57,6 @@ class Owner
   
   def sell_pets
     [:dogs, :cats, :fishes].each {|pet| mooder(pet,"nervous") }
-    mooder(:dogs,"nervous")
-    mooder(:cats,"happy")
-    mooder(:fishes,"happy")
    
    @pets.each do |type, pets|
      @pets[type] = []
